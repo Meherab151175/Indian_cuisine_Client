f431fbb565f878d8410aea595a62a5000e30092e
meherab151175 <meherabm803@gmail.com>
2023-06-01 08:06:33 +0600

    AuthProvider section edited

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-vars */
-// /* eslint-disable react/prop-types */
-// /* eslint-disable no-unused-vars */
 import React from "react";
 import { createContext } from "react";
 import {
